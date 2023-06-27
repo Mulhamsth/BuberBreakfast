68c1c5b170337a0fe563d146256961afac488e61
Mulhamsth <mulhamsth@gmail.com>
2023-06-27 20:39:21 +0200

    init commit for the project

---
+++ BuberBreakfast/Controllers/BreakfastController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using BuberBreakfast.Contracts.Breakfast;
+
+namespace BuberBreakfast.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BreakfastController : ControllerBase
+{
+    [HttpPost()]
+    public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+    {
+        return Ok(request);
+    }
+
+    [HttpGet("{id:guid}")]
+    public IActionResult GetBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+
+    [HttpPut("{id:guid}")]
+    public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequset requset)
+    {
+        return Ok(requset);
+    }
+
+    [HttpDelete("{id:guid}")]
+    public IActionResult DeleteBreakfast(Guid id)
+    {
+        return Ok(id);
+    }
+}
